6a31adc2b8489b14cbd6dee97888d45f06ae3645
caioireno <caio_ireno@hotmail.com>
2025-01-30 19:26:34 -0300

    feat: add CORS

--- api/config.py
+++ api/config.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['HOST'] = '0.0.0.0'
 app.config['PORT']=8000
 app.config['DEBUG'] = True
