41ccd9b7afbb1185379dcd9396d3bcd64f3827a7
Caio Ireno <100942876+caio-ireno@users.noreply.github.com>
2025-03-27 19:26:49 -0300

    Teste para deploy

--- api/alunos/alunos_model.py
+++ api/alunos/alunos_model.py
@@ -5,7 +5,8 @@ from config import db
 class Aluno(db.Model):
   __tablename__ = "alunos"
 
-   
+   asdasdasd
+
   id = db.Column(db.Integer, primary_key=True)
   nome = db.Column(db.String(100), nullable=False)
   idade = db.Column(db.Integer, nullable=False)
