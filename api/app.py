0cc946480a05fe45017ef3b7a6b0ac9046d8b311
caioireno <caio_ireno@hotmail.com>
2025-04-25 11:05:26 -0300

    fix: remove test

--- api/app.py
+++ api/app.py
@@ -16,16 +16,5 @@ configure_swagger(app)
 with app.app_context():
     db.create_all()
 
-def run_tests():
-    os.environ['FLASK_ENV'] = 'testing'
-    # Executa os testes e captura o resultado
-    result = pytest.main(['--maxfail=1', '--disable-warnings', '--tb=short'])
-    return result
-
 if __name__ == '__main__':
-    result = run_tests()
-    
-    if result != 0:  # Se algum teste falhar, não inicie o servidor
-        sys.exit("Testes falharam. A aplicação não será iniciada.")
-    
-    app.run(host=app.config["HOST"], port=app.config['PORT'], debug=app.config['DEBUG'])
+   app.run(host=app.config["HOST"], port=app.config['PORT'], debug=app.config['DEBUG'])
